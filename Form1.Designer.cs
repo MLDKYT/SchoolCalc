3bd401adc4cc71f53633f3651236c7c2576933ae
MLDKYT <mldkyt@protonmail.com>
2021-11-19 21:42:17 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace SchoolCalc3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Online";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(12, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(253, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "+1 online";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(356, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "+1 offline";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(275, 186);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 21);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "+1 homework/test";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(271, 78);
+            this.numericUpDown1.Maximum = new decimal(new int[] {1500, 0, 0, 0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {500, 0, 0, 0});
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(174, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Wattage per hour";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(437, 186);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(137, 21);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Export to txt file lol\r\n";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 219);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button5;
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace SchoolCalc3
+{
+    public partial class Form1 : Form
+    {
+        private bool _isOnline;
+        private float _onlineWattage;
+        private float _offlineWattage;
+        private int _onlineLessons;
+        private int _offlineLessons;
+        private int _homeWorkTestLessons;
+        private Dictionary<string, StringBuilder> informationsAboutEachLesson;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string path = Application.CommonAppDataPath + "/save2.xml";
+            if (File.Exists(path))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(SaveClass));
+                FileStream stream = File.Open(path, FileMode.Open);
+                SaveClass save = (SaveClass) serializer.Deserialize(stream);
+                _onlineWattage = save.Wattage.OnlineWattage;
+                _offlineWattage = save.Wattage.OfflineWattage;
+                _onlineLessons = save.Lessons.OnlineLessons;
+                _offlineLessons = save.Lessons.OfflineLessons;
+                _homeWorkTestLessons = save.Lessons.HomeWorkTestLessons;
+                numericUpDown1.Value = Decimal.Parse(save.Other.ComputerWattage.ToString(CultureInfo.CurrentCulture));
+                stream.Close();
+            }
+
+            button1.Text = @"Offline";
+            label1.Text = $@"Online: {_onlineWattage}W";
+            label2.Text = $@"Offline: {_offlineWattage}W";
+            label3.Text = $@"Online: {_onlineLessons}";
+            label4.Text = $@"Offline: {_offlineLessons}";
+            label5.Text = $@"Homework/Test instead of lesson: {_homeWorkTestLessons}";
+
+            Timer timer = new Timer {Interval = 1000};
+            timer.Tick += (sender, args) =>
+            {
+                if (_isOnline)
+                {
+                    _onlineWattage = _onlineWattage + float.Parse(numericUpDown1.Value.ToString(CultureInfo.CurrentCulture)) / 3600;
+                    label1.Text = @"Online: " + _onlineWattage + @"W";
+                }
+                else
+                {
+                    _offlineWattage = _offlineWattage + float.Parse(numericUpDown1.Value.ToString(CultureInfo.CurrentCulture)) / 3600;
+                    label2.Text = @"Offline: " + _offlineWattage + @"W";
+                }
+            };
+            timer.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _isOnline = !_isOnline;
+            button1.Text = _isOnline ? "Online" : "Offline";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _onlineLessons += 1;
+            label3.Text = @"Online: " + _onlineLessons;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _offlineLessons += 1;
+            label4.Text = @"Offline: " + _offlineLessons;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _homeWorkTestLessons += 1;
+            label5.Text = @"Homework/Test instead of lesson: " + _homeWorkTestLessons;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Save all the shit
+            WattageStuff wattageStuff = new WattageStuff(_onlineWattage, _offlineWattage);
+            LessonsStuff lessonsStuff = new LessonsStuff(_onlineLessons, _offlineLessons, _homeWorkTestLessons);
+            OtherStuff otherStuff = new OtherStuff(float.Parse(numericUpDown1.Value.ToString(CultureInfo.CurrentCulture)));
+            SaveClass saveStuff = new SaveClass(wattageStuff, lessonsStuff, otherStuff);
+
+            string path = Application.CommonAppDataPath + "/save2.xml";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(SaveClass));
+            if (File.Exists(path))
+                File.Delete(path);
+            FileStream stream = File.Create(path);
+            serializer.Serialize(stream, saveStuff);
+            stream.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("#############HLAVNI INFORMACE###################");
+            builder.AppendLine("###informace o elektrice#########");
+            builder.AppendLine("Při online                                                : " + _onlineWattage + "W");
+            builder.AppendLine("Ostatní (úkoly, přestávky, neexistující online hodiny)    : " + _offlineWattage + "W");
+            builder.AppendLine("");
+            builder.AppendLine("###informace o online hodinách###");
+            builder.AppendLine("Online hodiny                                             : " + _onlineLessons);
+            builder.AppendLine("Hodiny které vůbec neexistovali                           : " + _offlineLessons);
+            builder.AppendLine("Hodiny, na které se učitel vysr- dal nám úkol místo hodiny: " + _homeWorkTestLessons);
+            builder.AppendLine("");
+            builder.AppendLine("#############INFORMACE O HODINACH###############");
+            foreach (var variable in informationsAboutEachLesson)
+            {
+                builder.AppendLine(variable.Key + ": ");
+                builder.AppendLine(variable.Value.ToString());
+            }
+
+            builder.AppendLine("################################################");
+            builder.AppendLine("//Vygenerováno pomocí programu, který je používán na sledování");
+            builder.AppendLine("//online hodin. ");
+        }
+    }
+
+    [Serializable]
+    public class SaveClass
+    {
+        public SaveClass(WattageStuff wattage, LessonsStuff lessons, OtherStuff other)
+        {
+            Wattage = wattage;
+            Lessons = lessons;
+            Other = other;
+        }
+
+        public SaveClass()
+        {
+            // For serialization support you need to create an empty constructor
+        }
+
+        public WattageStuff Wattage;
+        public LessonsStuff Lessons;
+        public OtherStuff Other;
+    }
+
+    [Serializable]
+    public class WattageStuff
+    {
+        public WattageStuff(float onlineWattage, float offlineWattage)
+        {
+            OnlineWattage = onlineWattage;
+            OfflineWattage = offlineWattage;
+        }
+
+        public WattageStuff()
+        {
+            // For serialization support you need to create an empty constructor
+        }
+
+        public float OnlineWattage;
+        public float OfflineWattage;
+
+        public void SetOnlineWattages(float onlineWattage, float offlineWattage)
+        {
+            OnlineWattage = onlineWattage;
+            OfflineWattage = offlineWattage;
+        }
+    }
+
+    [Serializable]
+    public class LessonsStuff
+    {
+        public LessonsStuff(int onlineLessons, int offlineLessons, int homeWorkTestLessons)
+        {
+            OnlineLessons = onlineLessons;
+            OfflineLessons = offlineLessons;
+            HomeWorkTestLessons = homeWorkTestLessons;
+        }
+
+        public LessonsStuff()
+        {
+            // For serialization support you need to create an empty constructor
+        }
+
+        public int OnlineLessons;
+        public int OfflineLessons;
+        public int HomeWorkTestLessons;
+
+        public void SetLessons(int onlineLessons, int offlineLessons, int homeWorkTestLessons)
+        {
+            OnlineLessons = onlineLessons;
+            OfflineLessons = offlineLessons;
+            HomeWorkTestLessons = homeWorkTestLessons;
+        }
+    }
+
+    public class OtherStuff
+    {
+        public OtherStuff(float computerWattage)
+        {
+            ComputerWattage = computerWattage;
+        }
+
+        public OtherStuff()
+        {
+            // For serialization support you need to create an empty constructor
+        }
+
+        public float ComputerWattage;
+    }
+}
